817342cf597f49d6a118604473e804eb187a456a
Alejandro Mejía <alejandromejia2013.27@gmail.com>
2025-05-13 00:31:49 -0300

    refactor(repositories): add missing exception handling import to repository classes

--- CentroEventos.Repositorios/Repositorios/RepositorioReserva.cs
+++ CentroEventos.Repositorios/Repositorios/RepositorioReserva.cs
@@ -1,5 +1,6 @@
 using CentroEventos.Aplicacion.Entidades;
 using CentroEventos.Aplicacion.Interfaces;
+using CentroEventos.Aplicacion.Excepciones;
 
 namespace CentroEventos.Repositorios.Repositorios;
 
