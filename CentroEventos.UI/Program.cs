22241e6294ccb9e34c7993907f00d6b4498d8c48
Franciskoxz <fran_mayo_boca@hotmail.com>
2025-06-19 20:15:06 -0300

    inject deps

--- CentroEventos.UI/Program.cs
+++ CentroEventos.UI/Program.cs
@@ -1,10 +1,64 @@
 using CentroEventos.UI.Components;
 
+using CentroEventos.Repositorios;
+using CentroEventos.Aplicacion.Entidades;
+using CentroEventos.Aplicacion.Validadores;
+using CentroEventos.Aplicacion.Interfaces;
+
+using CentroEventos.Aplicacion.CasosDeUso;
+using CentroEventos.Aplicacion.CasosDeUso.UsuarioUseCases;
+using CentroEventos.Aplicacion.CasosDeUso.PersonaUseCases;
+using CentroEventos.Aplicacion.CasosDeUso.EventoDeportivoUseCases;
+using CentroEventos.Aplicacion.CasosDeUso.ReservaUseCases;
+
+using CentroEventos.Repositorios.Repositorios;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+// Casos de uso - Usuario
+builder.Services.AddTransient<UsuarioCrearUseCase>();
+builder.Services.AddTransient<UsuarioActualizarUseCase>();
+builder.Services.AddTransient<UsuarioEliminarUseCase>();
+builder.Services.AddTransient<UsuarioLoginUseCase>();
+builder.Services.AddTransient<UsuarioObtenerPorIdUseCase>();
+builder.Services.AddTransient<UsuarioObtenerTodosUseCase>();
+
+// Casos de uso - Persona
+builder.Services.AddTransient<PersonaCrearUseCase>();
+builder.Services.AddTransient<PersonaActualizarUseCase>();
+builder.Services.AddTransient<PersonaEliminarUseCase>();
+builder.Services.AddTransient<PersonaObtenerPorIdUseCase>();
+builder.Services.AddTransient<PersonaObtenerTodosUseCase>();
+
+// Casos de uso - Evento Deportivo
+builder.Services.AddTransient<EventoDeportivoCrearUseCase>();
+builder.Services.AddTransient<EventoDeportivoActualizarUseCase>();
+builder.Services.AddTransient<EventoDeportivoEliminarUseCase>();
+builder.Services.AddTransient<EventoDeportivoObtenerPorIdUseCase>();
+builder.Services.AddTransient<EventoDeportivoObtenerTodosUseCase>();
+
+// Casos de uso - Reserva
+builder.Services.AddTransient<ReservaAltaUseCase>();
+builder.Services.AddTransient<ReservaActualizarUseCase>();
+builder.Services.AddTransient<ReservaEliminarUseCase>();
+builder.Services.AddTransient<ReservaObtenerPorIdUseCase>();
+builder.Services.AddTransient<ReservaObtenerTodosUseCase>();
+
+// Repositorios
+builder.Services.AddScoped<IRepositorioUsuario, RepositorioUsuario>();
+builder.Services.AddScoped<IRepositorioPersona, RepositorioPersona>();
+builder.Services.AddScoped<IRepositorioEventoDeportivo, RepositorioEventoDeportivo>();
+builder.Services.AddScoped<IRepositorioReserva, RepositorioReserva>();
+
+// Validadores
+builder.Services.AddScoped<ValidadorPersona>();
+builder.Services.AddScoped<ValidadorEventoDeportivo>();
+builder.Services.AddScoped<ValidadorReserva>();
+builder.Services.AddScoped<ValidadorUsuario>();
 
 var app = builder.Build();
 
